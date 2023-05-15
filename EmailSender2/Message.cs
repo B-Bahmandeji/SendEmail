7beed8b646e1113fc92a254c02ac2de3b8f82d78
bahmanbj <bahmanbj@yahoo.com>
2023-05-15 14:54:58 -0400

    start point
    how to set a class library in Asp.net Core

---
+++ EmailSender2/Message.cs
@@ -0,0 +1,35 @@
+﻿using MimeKit;
+
+namespace EmailService
+{
+    public class Message : MimeMessage
+    {
+        public List<MailboxAddress> To { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public Message(IEnumerable<string> to, string subject, string content)
+        {
+            To = new List<MailboxAddress>();
+            To.AddRange(to.Select(x => new MailboxAddress("",x)));//""=email sender name
+            Subject = subject;
+            Content = content;
+        }
+        public Message(IEnumerable<MailboxAddress> to, string subject, string content)
+        {
+            To = new List<MailboxAddress>();
+            To.AddRange(to.Select(x => new MailboxAddress(x.Name, x.Address)));//key=name, value=email
+            Subject = subject;
+            Content = content;
+        }
+        public Message(string name, string address, string subject, string content)
+        {
+            //List<MailboxAddress> mbato = new List<MailboxAddress>();
+            //mbato.Add(new MailboxAddress(name, address));
+            To = new List<MailboxAddress>();
+            To.Add(new MailboxAddress(name, address));
+            //To.AddRange(mbato.Select(x => new MailboxAddress(x.Name, x.Address)));//key=name, value=email
+            Subject = subject;
+            Content = content;
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,1 @@
+Here I show you how to create a Class Library project with Asp.Net Core and then you can add to it as a dll to any regular Asp.net Core project and send email from your web site by Gmail
